4fb57e183e5614b67a04a2da2f7acdb667eb512f
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-01 13:18:51 +0300

    get relation collection

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Question;
+use App\Repository\AnswerRepository;
 use App\Repository\QuestionRepository;
 use App\Service\MarkdownHelper;
 use Doctrine\ORM\EntityManagerInterface;
@@ -53,11 +54,7 @@ class QuestionController extends AbstractController
             $this->logger->info('We are in debug mode!');
         }
 
-        $answers = [
-            'Make sure your cat is sitting `purrrfectly` still 🤣',
-            'Honestly, I like furry shoes better than MY cat',
-            'Maybe... try saying the spell backwards?',
-        ];
+        $answers = $question->getAnswers();
 
         return $this->render('question/show.html.twig', [
             'question' => $question,
